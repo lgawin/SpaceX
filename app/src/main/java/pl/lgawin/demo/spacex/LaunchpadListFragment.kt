1534afa56d39d3c4d59738e8f100dac0babced68
Łukasz Gawin <lukasz.gawin@gmail.com>
2021-12-23 12:53:42 +0100

    Create dedicated fragment for launchpad list

---
+++ app/src/main/java/pl/lgawin/demo/spacex/LaunchpadListFragment.kt
@@ -0,0 +1,5 @@
+package pl.lgawin.demo.spacex
+
+import androidx.fragment.app.Fragment
+
+class LaunchpadListFragment : Fragment(R.layout.fragment_launchpad_list)
--- app/src/main/java/pl/lgawin/demo/spacex/MainActivity.kt
+++ app/src/main/java/pl/lgawin/demo/spacex/MainActivity.kt
@@ -2,6 +2,8 @@ package pl.lgawin.demo.spacex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import pl.lgawin.demo.spacex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_SpaceX)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view, LaunchpadListFragment::class.java, null)
+        }
     }
 }
