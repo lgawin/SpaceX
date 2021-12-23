2fb58d4eff7ecfd0f0932a886cd20307f08d32ae
Łukasz Gawin <lukasz.gawin@gmail.com>
2021-12-23 11:08:35 +0100

    Well-styled splash screen
    * Android-12 & legacy mode
    * light & dark mode supported
    * icon style fixed

--- app/src/main/java/pl/lgawin/demo/spacex/MainActivity.kt
+++ app/src/main/java/pl/lgawin/demo/spacex/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_SpaceX)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
